25ee286871399fdd454b6e39dde0dad90fb9131d
jiang zhu <57114034@qq.com>
2019-05-09 14:55:02 +0800

    修改

--- app/src/main/java/zj/it/bhne/androidaop/Main2Activity.java
+++ app/src/main/java/zj/it/bhne/androidaop/Main2Activity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.blankj.utilcode.util.ToastUtils;
+import com.zj.singclick.SingleClick;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
--- app/src/main/java/zj/it/bhne/androidaop/MainActivity.kt
+++ app/src/main/java/zj/it/bhne/androidaop/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import com.blankj.utilcode.util.ToastUtils
+import com.zj.singclick.SingleClick
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() ,View.OnClickListener{
