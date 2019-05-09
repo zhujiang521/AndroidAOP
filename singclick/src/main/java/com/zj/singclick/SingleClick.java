56f40835e8cd122f4807ae34863fe112ab32a44d
jiang zhu <57114034@qq.com>
2019-05-09 14:44:17 +0800

    添加Android Library

---
+++ singclick/src/main/java/com/zj/singclick/SingleClick.java
@@ -0,0 +1,16 @@
+package com.zj.singclick;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author jiang zhu on 2019/4/19
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SingleClick {
+    /* 点击间隔时间 */
+    long value() default 1500;
+}
---
+++ singclick/src/main/java/com/zj/singclick/XClickUtil.java
@@ -0,0 +1,38 @@
+package com.zj.singclick;
+
+import android.view.View;
+
+/**
+ * @author jiang zhu on 2019/4/19
+ */
+public final class XClickUtil {
+
+    /**
+     * 最近一次点击的时间
+     */
+    private static long mLastClickTime;
+    /**
+     * 最近一次点击的控件ID
+     */
+    private static int mLastClickViewId;
+
+    /**
+     * 是否是快速点击
+     *
+     * @param v  点击的控件
+     * @param intervalMillis  时间间期（毫秒）
+     * @return  true:是，false:不是
+     */
+    public static boolean isFastDoubleClick(View v, long intervalMillis) {
+        int viewId = v.getId();
+        long time = System.currentTimeMillis();
+        long timeInterval = Math.abs(time - mLastClickTime);
+        if (timeInterval < intervalMillis && viewId == mLastClickViewId) {
+            return true;
+        } else {
+            mLastClickTime = time;
+            mLastClickViewId = viewId;
+            return false;
+        }
+    }
+}
