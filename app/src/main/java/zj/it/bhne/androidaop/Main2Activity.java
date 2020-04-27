8d7fff446ab04ef4c0fcc115b8092da20b498d3e
zhujiang521 <57114034@qq.com>
2020-04-27 13:39:01 +0800

    新增几种处理方式

--- app/src/main/java/zj/it/bhne/androidaop/Main2Activity.java
+++ app/src/main/java/zj/it/bhne/androidaop/Main2Activity.java
@@ -1,5 +1,6 @@
 package zj.it.bhne.androidaop;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,32 +12,48 @@ import com.blankj.utilcode.util.ToastUtils;
 import com.zj.singclick.SingleClick;
 
 
+public class Main2Activity extends AppCompatActivity{
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+    private final static String TAG = "Main2Activity";
+    //全局定义
+    private long lastClickTime = 0L;
+    // 两次点击间隔不能少于1000ms
+    private static final int FAST_CLICK_DELAY_TIME = 1000;
 
-    private Button zhu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        initView();
     }
 
-    private void initView() {
-        zhu = findViewById(R.id.zhu);
+    /**
+     * 弹窗等待
+     *
+     * @param view /
+     */
+    public void btnDialog(View view) {
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("等待");
+        progressDialog.setMessage("等待内容");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
 
-        zhu.setOnClickListener(this);
     }
 
-    @SingleClick
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.zhu:
-                ToastUtils.showShort("sssss");
-                Log.e("刘佩希","wwww");
-                break;
+    public void btnStop(View view) {
+    }
+
+    public void btnInter(View view) {
+        if (System.currentTimeMillis() - lastClickTime >= FAST_CLICK_DELAY_TIME) {
+            lastClickTime = System.currentTimeMillis();
         }
     }
+
+    @SingleClick(2000)
+    public void btnAop(View view) {
+        ToastUtils.showShort("btnAop");
+        Log.e(TAG, "btnAop");
+    }
+
 }
